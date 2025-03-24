4f6642e64296b97d29fc68ee6e666907ea507390
Alok Singh <alok.singh@publicissapient.com>
2025-03-24 22:57:27 +0530

    file added

---
+++ HelloJen.java
@@ -0,0 +1,7 @@
+public class HelloJen {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins the first program :)");
+        System.out.println("running............ ");
+    }
+}
