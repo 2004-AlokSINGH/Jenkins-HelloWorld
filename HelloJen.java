4a8e79396ea51c06deb62608773effbb60da49fa
ALOK SINGH <123860933+2004-AlokSINGH@users.noreply.github.com>
2025-03-24 23:15:51 +0530

    Update HelloJen.java

--- HelloJen.java
+++ HelloJen.java
@@ -2,7 +2,7 @@ public class HelloJen {
 
     public static void main(String[] args) {
         System.out.println("Hello Jenkins the first program :)");
-        System.out.println("running......code changes    ");
+        System.out.println("running......    ");
         System.out.println("adding new code............ ");
         
     }
